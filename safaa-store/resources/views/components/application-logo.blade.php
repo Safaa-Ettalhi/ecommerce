ea0fad8992a9460e55a6e3a67510fc5f0eee31e0
Safaa-Ettalhi <safaeettalhi@gmail.com>
2025-03-08 22:27:02 +0000

    add views component

---
+++ safaa-store/resources/views/components/application-logo.blade.php
@@ -0,0 +1,23 @@
+<div {{ $attributes->merge(['class' => 'flex items-center']) }}>
+    <div class="text-2xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500">
+        <span class="flex items-center">
+            <svg class="w-8 h-8 mr-2" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M12 2L2 7L12 12L22 7L12 2Z" fill="url(#gradient1)"/>
+                <path d="M2 17L12 22L22 17M2 12L12 17L22 12" stroke="url(#gradient2)" stroke-width="2"/>
+                <defs>
+                    <linearGradient id="gradient1" x1="2" y1="2" x2="22" y2="12" gradientUnits="userSpaceOnUse">
+                        <stop stop-color="#EC4899"/>
+                        <stop offset="0.5" stop-color="#8B5CF6"/>
+                        <stop offset="1" stop-color="#6366F1"/>
+                    </linearGradient>
+                    <linearGradient id="gradient2" x1="2" y1="12" x2="22" y2="22" gradientUnits="userSpaceOnUse">
+                        <stop stop-color="#EC4899"/>
+                        <stop offset="0.5" stop-color="#8B5CF6"/>
+                        <stop offset="1" stop-color="#6366F1"/>
+                    </linearGradient>
+                </defs>
+            </svg>
+            Safaa
+        </span>
+    </div>
+</div>
